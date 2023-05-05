a0b3e6df3bddb7a92fa9b12f699e4d632e989dfe
bxbao87 <bxbao18@apcs.vn>
2023-05-05 13:02:20 +0700

    [part5a] ex5.2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
+const loggedKey = "loggedUser"
+
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [username, setUsername] = useState([])
@@ -15,6 +17,14 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem(loggedKey)
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+    }
+  }, [])
+
   const handleLogin = async (event) => {
     event.preventDefault()
 
@@ -22,6 +32,10 @@ const App = () => {
       const user = await loginService.login({
         username, password
       })
+
+      window.localStorage.setItem(
+        loggedKey, JSON.stringify(user)
+      )
 
       setUser(user)
       setUsername('')
@@ -62,10 +76,17 @@ const App = () => {
     return loginForm()
   }
 
+  const handleLogout = (event) => {
+    window.localStorage.removeItem(loggedKey)
+    setUser(null)
+  }
+
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in</p>
+      <p>
+        {user.name} logged in <button onClick={handleLogout}>logout</button>
+      </p>
 
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
